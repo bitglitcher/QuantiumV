84bb4f42ac09bda08d25707d06d92c27385fecab
Benjamin Herrera <caminlobo@gmail.com>
2023-06-10 10:38:42 -0500

    Setup Simulation clock and reset

---
+++ src/debug_def.sv
@@ -0,0 +1,10 @@
+`ifndef __DEBUG__PORT__SV__
+`define __DEBUG__PORT__SV__
+
+`ifndef ALTERA_RESERVED_QIS
+    `define __sim__
+    `define DEBUG_PORT
+    `define MEMORY_DEBUG_MSGS
+`endif
+
+`endif
--- src/soc.sv
+++ src/soc.sv
@@ -1,5 +1,71 @@
-module soc();
-  
-  
-  
+`timescale 1ps/1ps
+
+`include "debug_def.sv"
+
+module soc
+(
+    `ifndef __sim__
+    input logic clk, 
+    input logic rst
+    `endif
+);
+
+//Internal RAM for BOOT ROM
+parameter SDRAM_LENGHT = 4096;
+
+//Wishobne interface
+logic        ACK;
+logic        ERR;
+logic        RTY;
+logic        STB;
+logic        CYC;
+logic [31:0] ADR;
+logic [31:0] DAT_I;
+logic [31:0] DAT_O;
+logic [2:0]  CTI_O;
+logic        WE;
+
+//Generated clock signal for out simulation
+logic clk_gen = 0;
+
+//Synchronizer for the RST signal to debounce the input signal.
+logic rst_gen = 0;
+always@(posedge clk_gen)
+begin
+    rst_gen = ~rst;
+end
+
+
+`ifdef __sim__
+
+logic clk;
+logic rst;
+initial begin
+    $dumpfile("soc.vcd");
+    $dumpvars(0,soc);
+    $display("Initializing Simulations");
+    clk_gen = 0;
+    //RST Starts at a value of 1 to simulate the pull up resistor
+    //for the inputs in a lot of FPGA boards.
+    rst = 1;
+    #10
+    clk_gen = ~clk_gen;
+    rst = 0;
+    repeat(10)
+    begin
+        #10
+        begin
+            clk_gen = ~clk_gen;
+        end
+    end
+    rst = 1;
+    forever begin
+        #10
+		begin
+			clk_gen = ~clk_gen;
+    	end
+	end
+end
+`endif
+
 endmodule
