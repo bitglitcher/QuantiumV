7982eb3c55b7a96cf2765a0691083a1452a222a6
Benjamin Herrera <caminlobo@gmail.com>
2023-06-10 11:03:52 -0500

    Declare IO for Cache and Fetch Stage

--- src/fetch_stage.sv
+++ src/fetch_stage.sv
@@ -1,5 +1,45 @@
-module fetch_stage();
+ /*
+ * ++============================================================
+ * || ++============================================++     +----+
+ * || ||                           +-+   +------+   ||     |    |
+ * || ||                           |4|==>| A    |   ||     |    |
+ * || ||                           +-+   | +    |===++     |    |
+ * || ||                           ++===>| B    |          |    |
+ * || ||                           ||    +------+          |    |
+ * || ||                           ||                      |    |
+ * || ||    +------+    +------+   ||    +-----------+     | IF |
+ * || ++===>| 0    |    |      |   ||    |           |     |    |
+ * ||       | MUX  |===>|  PC  |===++===>|  I Cache  |====>|    |
+ * ++======>| 1    |    |      |  ADDR   |           |     |    |
+ *          +------+    +------+         +-----------+     |    |
+ *                                       |  AXI/Wb4  |     |    |
+ *                                       +-----------+     |    |
+ *                                             /\          |    |
+ *                                             || I BUS    |    |
+ *                                             \/          +----+
+ */
 
+module fetch_stage
+(
+    input logic clk,
+    input logic rst,
+    //Master Wishbone interface
+    input  logic ACK,
+    input  logic ERR,
+    input  logic RTY,
+    output logic STB,
+    output logic CYC,
+    output logic [31:0] ADR,
+    input  logic [31:0] DAT_I,
+    output logic [31:0] DAT_O,
+    output logic [2:0]  CTI_O,
+    output logic WE,
+    //Stage Outputs
+    output logic [31:0] INS //Instruction Data 
+);
+
+
+reg PC = 0;
 
 
 endmodule
--- src/i_cache.sv
+++ src/i_cache.sv
@@ -1,4 +1,22 @@
-module i_cache();
+module i_cache
+(
+    input logic clk,
+    input logic rst,
+    //Master Wishbone interface
+    input  logic ACK,
+    input  logic ERR,
+    input  logic RTY,
+    output logic STB,
+    output logic CYC,
+    output logic [31:0] ADR,
+    input  logic [31:0] DAT_I,
+    output logic [31:0] DAT_O,
+    output logic [2:0]  CTI_O,
+    output logic WE,
+    //Output Data
+    output logic [31:0] INS,
+    output logic stall
+);
 
 
 
