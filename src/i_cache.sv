5b6bfff637f02dff75a2e522feb54bd9d1f3e64e
Benjamin Herrera <caminlobo@gmail.com>
2023-06-19 00:20:56 -0500

    define some of the structures in the cache

--- src/i_cache.sv
+++ src/i_cache.sv
@@ -1,3 +1,4 @@
+//4 Way associative Cache
 module i_cache
 (
     input logic clk,
@@ -19,5 +20,48 @@ module i_cache
 );
 
 
+typedef enum logic [1:0] { 
+    IDDLE, 
+    CACHE_HIT, 
+    CACHE_MISS, 
+    CACHE_LOAD_MISSALIGNED_B1,
+    CACHE_LOAD_MISSALIGNED_B2,
+    CACHE_WRITE_BACK_B1
+    CACHE_WRITE_BACK_B2
+} cache_state_t;
+
+//Associative Table Entry
+//+---+---+-----+---------------+
+//| V | D | TAG | DATA 32 Bytes |
+//+---+---+-----+---------------+
+
+//      Addressing
+//+--------+-----+-----+
+//|  TAG   | ATI | WA  |
+//+--+-----+-----+-----+
+//
+// WA  - Word Address. 3 bits. Ignored in Cache logic.
+// ATI - Associative Table Index. 2 bits.
+// TAG - 28 bit
+
+//1KB per table
+reg [28:0] TAG_LVL1 [127:0];
+reg [28:0] TAG_LVL2 [127:0];
+reg [28:0] TAG_LVL3 [127:0];
+reg [28:0] TAG_LVL4 [127:0];
+reg V_BIT [127:0];
+reg V_BIT [127:0];
+reg V_BIT [127:0];
+reg V_BIT [127:0];
+reg V_BIT [127:0];
+reg V_BIT [127:0];
+reg V_BIT [127:0];
+reg V_BIT [127:0];
+
+
+always@(posedge clk or posedge rst)
+begin
+    
+end
 
 endmodule
